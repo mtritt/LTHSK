f8b7dfbef59a7992bd3473af0d046f59d5c6dde7
Đỗ Minh Trí <dominhtri2572006@gmail.com>
2026-03-30 02:05:36 +0700

    reupdate full file sql

--- QuanLyCuaHangQuanAo.sql
+++ QuanLyCuaHangQuanAo.sql
@@ -223,6 +223,95 @@ INSERT INTO tblSanPhamChiTiet VALUES
 ('SPCT06','SP05','L','DEN',500000,15),
 ('SPCT07','SP06','S','XANH',180000,60);
 
+INSERT INTO tblSanPhamChiTiet VALUES 
+('SPCT08', 'SP01', 'S', 'DEN',   190000, 35),
+('SPCT09', 'SP01', 'L', 'DEN',   210000, 20),
+('SPCT10', 'SP01', 'S', 'TRANG', 190000, 45),
+('SPCT11', 'SP01', 'M', 'TRANG', 200000, 50),
+('SPCT12', 'SP01', 'M', 'DO',    195000, 40),
+('SPCT13', 'SP01', 'L', 'XANH',  205000, 25),
+('SPCT14', 'SP02', 'S', 'DEN',   340000, 25),
+('SPCT15', 'SP02', 'L', 'TRANG', 360000, 20),
+('SPCT16', 'SP02', 'M', 'DO',    350000, 10),
+
+-- Bổ sung phân loại cho SP03 (Áo sơ mi)
+('SPCT17', 'SP03', 'M', 'DEN',   250000, 30),
+('SPCT18', 'SP03', 'L', 'XANH',  270000, 15),
+
+-- Bổ sung phân loại cho SP04 (Váy nữ)
+('SPCT19', 'SP04', 'S', 'DEN',   390000, 20),
+('SPCT20', 'SP04', 'L', 'TRANG', 410000, 10),
+
+-- Bổ sung phân loại cho SP05 (Áo khoác)
+('SPCT21', 'SP05', 'M', 'TRANG', 490000, 15),
+('SPCT22', 'SP05', 'S', 'XANH',  480000, 25),
+
+-- Bổ sung phân loại cho SP06 (Quần short)
+('SPCT23', 'SP06', 'M', 'DEN',   180000, 40),
+('SPCT24', 'SP06', 'L', 'DO',    190000, 35);
+INSERT INTO tblSanPhamChiTiet (MaSPCT, MaSP, MaSize, MaMau, Giaban, SoLuongTon)
+VALUES 
+-- Bổ sung 4 loại thiếu cho SP01 (Áo thun nam)
+('SPCT25', 'SP01', 'S', 'DO',    190000, 30),
+('SPCT26', 'SP01', 'S', 'XANH',  190000, 25),
+('SPCT27', 'SP01', 'M', 'XANH',  200000, 30),
+('SPCT28', 'SP01', 'L', 'DO',    210000, 20),
+
+-- Bổ sung 8 loại thiếu cho SP02 (Quần jean nữ)
+('SPCT29', 'SP02', 'S', 'DO',    340000, 20),
+('SPCT30', 'SP02', 'S', 'TRANG', 340000, 25),
+('SPCT31', 'SP02', 'S', 'XANH',  340000, 15),
+('SPCT32', 'SP02', 'M', 'DEN',   350000, 30),
+('SPCT33', 'SP02', 'M', 'TRANG', 350000, 20),
+('SPCT34', 'SP02', 'L', 'DEN',   360000, 15),
+('SPCT35', 'SP02', 'L', 'DO',    360000, 10),
+('SPCT36', 'SP02', 'L', 'XANH',  360000, 20),
+
+-- Bổ sung 9 loại thiếu cho SP03 (Áo sơ mi)
+('SPCT37', 'SP03', 'S', 'DEN',   240000, 25),
+('SPCT38', 'SP03', 'S', 'DO',    240000, 20),
+('SPCT39', 'SP03', 'S', 'XANH',  240000, 15),
+('SPCT40', 'SP03', 'M', 'DO',    250000, 25),
+('SPCT41', 'SP03', 'M', 'TRANG', 250000, 30),
+('SPCT42', 'SP03', 'M', 'XANH',  250000, 20),
+('SPCT43', 'SP03', 'L', 'DEN',   260000, 15),
+('SPCT44', 'SP03', 'L', 'DO',    260000, 10),
+('SPCT45', 'SP03', 'L', 'TRANG', 260000, 20),
+
+-- Bổ sung 9 loại thiếu cho SP04 (Váy nữ)
+('SPCT46', 'SP04', 'S', 'DO',    390000, 25),
+('SPCT47', 'SP04', 'S', 'TRANG', 390000, 20),
+('SPCT48', 'SP04', 'S', 'XANH',  390000, 15),
+('SPCT49', 'SP04', 'M', 'DEN',   400000, 30),
+('SPCT50', 'SP04', 'M', 'TRANG', 400000, 25),
+('SPCT51', 'SP04', 'M', 'XANH',  400000, 20),
+('SPCT52', 'SP04', 'L', 'DEN',   410000, 15),
+('SPCT53', 'SP04', 'L', 'DO',    410000, 10),
+('SPCT54', 'SP04', 'L', 'XANH',  410000, 20),
+
+-- Bổ sung 9 loại thiếu cho SP05 (Áo khoác)
+('SPCT55', 'SP05', 'S', 'DEN',   480000, 20),
+('SPCT56', 'SP05', 'S', 'DO',    480000, 15),
+('SPCT57', 'SP05', 'S', 'TRANG', 480000, 25),
+('SPCT58', 'SP05', 'M', 'DEN',   490000, 30),
+('SPCT59', 'SP05', 'M', 'DO',    490000, 20),
+('SPCT60', 'SP05', 'M', 'XANH',  490000, 15),
+('SPCT61', 'SP05', 'L', 'DO',    500000, 10),
+('SPCT62', 'SP05', 'L', 'TRANG', 500000, 15),
+('SPCT63', 'SP05', 'L', 'XANH',  500000, 20),
+
+-- Bổ sung 9 loại thiếu cho SP06 (Quần short)
+('SPCT64', 'SP06', 'S', 'DEN',   170000, 30),
+('SPCT65', 'SP06', 'S', 'DO',    170000, 25),
+('SPCT66', 'SP06', 'S', 'TRANG', 170000, 20),
+('SPCT67', 'SP06', 'M', 'DO',    180000, 20),
+('SPCT68', 'SP06', 'M', 'TRANG', 180000, 25),
+('SPCT69', 'SP06', 'M', 'XANH',  180000, 15),
+('SPCT70', 'SP06', 'L', 'DEN',   190000, 20),
+('SPCT71', 'SP06', 'L', 'TRANG', 190000, 15),
+('SPCT72', 'SP06', 'L', 'XANH',  190000, 25);
+
+
 --tblPhieuNhap
 INSERT INTO tblPhieuNhap VALUES
 ('PN01','NCC01',1,GETDATE(),0),
@@ -272,3 +361,8 @@ select * from tblHoaDonBan
 select * from tblNhanVien
 select * from tblChiTietHDB
 select * from tblSanPhamChiTiet
+
+SELECT * FROM tblSanPhamChiTiet
+ORDER BY MaSP ASC;
+SELECT COUNT(*) FROM tblSanPhamChiTiet
+
